70ccfa9bb01f7cfb880ab39f124cfd38a9df8772
nergis ketenci <nrgsktnc@hotmail.com>
2023-10-24 19:13:26 +0300

    changes

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -13,13 +13,21 @@ namespace Loops
             // Döngüler veri kümesinin içerisinde dolaşmak için kullandığımız yapılardır.
             // for(başlangıç değeri,şart,artış)
 
-            for (int i = 100; i >= 0; i-=2)
+            //for (int i = 100; i >= 0; i-=2)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.WriteLine("Finished!");
+
+
+            // While Döngüsü: Belli bir şart sağlanana kadar döngü çalışır.
+            int number = 100;
+            while (number>=0)
             {
-                Console.WriteLine(i);
+                Console.WriteLine(number);
+                number--;
+                // Sonsuz döngü olmaması için döngünün içerisinde şarta müdahale etmek gerekir.
             }
-            Console.WriteLine("Finished!");
-
-
 
             Console.ReadLine();
         }
