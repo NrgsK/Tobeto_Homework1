86731f7e5b0307c9f8ce230ed26dbf3b041bde65
nergis ketenci <nrgsktnc@hotmail.com>
2023-10-24 19:57:25 +0300

    changes

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -12,23 +12,41 @@ namespace Strings
         {
             //string metinsel verileri tutmak için kullanılan değişken tipidir.
 
-            string city = "Ankara";
+            //string city = "Ankara";
 
             // stringler aslında char dizisidir.
             // Console.WriteLine(city[0]);
 
-            foreach (var item in city)
-            {
-                Console.WriteLine(item);
+            //foreach (var item in city)
+            //{
+            //    Console.WriteLine(item);
 
-            }
+            //}
 
-            string city2 = "İstanbul";
+            //string city2 = "İstanbul";
             //string result = city + city2;
 
-            Console.WriteLine(String.Format("{0} {1}",city,city2));
+            //Console.WriteLine(String.Format("{0} {1}",city,city2));
+
+            string sentence = "My name is Nergis Ketenci";
+            var result = sentence.Length;
+            var result2 = sentence.Clone();
+            // Yeni bir referans oluşturduk.
+            sentence = "My name is Rukiye Ketenci";
+
+            bool result3 = sentence.EndsWith("g");
+            bool result4 = sentence.StartsWith("g");
+
+            var result5 = sentence.IndexOf("name");
+            // IndexOf bir string değişkende belli bir karakteri yada ifadeyi aramak için kullanılır. Aranan karakter bulunamazsa "-1" değeri döner.
 
 
+
+            Console.WriteLine(result);
+            Console.WriteLine(result2);
+            Console.WriteLine(result3);
+            Console.WriteLine(result4);
+            Console.WriteLine(result5);
             Console.ReadLine();
         }
     }
