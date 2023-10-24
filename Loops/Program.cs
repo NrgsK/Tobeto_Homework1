87d5a8eeced0949f1f2307229b813e5d171f6808
nergis ketenci <nrgsktnc@hotmail.com>
2023-10-24 19:09:52 +0300

    changes

---
+++ Loops/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Döngüler veri kümesinin içerisinde dolaşmak için kullandığımız yapılardır.
+            // for(başlangıç değeri,şart,artış)
+
+            for (int i = 100; i >= 0; i-=2)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Finished!");
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
